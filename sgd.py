986af600a24d22680cc6397f05998b5e7049f19c
Szymon Wojciechowski <bluetapeon@gmail.com>
2018-11-18 22:45:37 +0100

    reformat file

--- sgd.py
+++ sgd.py
@@ -2,78 +2,81 @@ import matplotlib.pyplot as plt
 from sklearn.datasets.samples_generator import make_blobs
 import numpy as np
 import argparse
- 
+
+
 def sigmoid_activation(x):
-	return 1.0 / (1 + np.exp(-x))
- 
+    return 1.0 / (1 + np.exp(-x))
+
+
 def next_batch(X, y, batchSize):
-	for i in np.arange(0, X.shape[0], batchSize):
-		yield (X[i:i + batchSize], y[i:i + batchSize])
+    for i in np.arange(0, X.shape[0], batchSize):
+        yield (X[i:i + batchSize], y[i:i + batchSize])
+
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-e", "--epochs", type=float, default=100,
-	help="# of epochs")
+                help="# of epochs")
 ap.add_argument("-a", "--alpha", type=float, default=0.01,
-	help="learning rate")
+                help="learning rate")
 ap.add_argument("-b", "--batch-size", type=int, default=32,
-	help="size of SGD mini-batches")
+                help="size of SGD mini-batches")
 args = vars(ap.parse_args())
 
 (X, y) = make_blobs(n_samples=400, n_features=2, centers=2,
-	cluster_std=2.5, random_state=95)
+                    cluster_std=2.5, random_state=95)
 
 X = np.c_[np.ones((X.shape[0])), X]
 
 print("[INFO] starting training...")
 W = np.random.uniform(size=(X.shape[1],))
- 
+
 # initialize a list to store the loss value for each epoch
 lossHistory = []
 
 # loop over the desired number of epochs
 for epoch in np.arange(0, args["epochs"]):
-	# initialize the total loss for the epoch
-	epochLoss = []
- 
-	# loop over our data in batches
-	for (batchX, batchY) in next_batch(X, y, args["batch_size"]):
-		# take the dot product between our current batch of
-		# features and weight matrix `W`, then pass this value
-		# through the sigmoid activation function
-		preds = sigmoid_activation(batchX.dot(W))
- 
-		# now that we have our predictions, we need to determine
-		# our `error`, which is the difference between our predictions
-		# and the true values
-		error = preds - batchY
- 
-		# given our `error`, we can compute the total loss value on
-		# the batch as the sum of squared loss
-		loss = np.sum(error ** 2)
-		epochLoss.append(loss)
- 
-		# the gradient update is therefore the dot product between
-		# the transpose of our current batch and the error on the
-		# # batch
-		gradient = batchX.T.dot(error) / batchX.shape[0]
- 
-		# use the gradient computed on the current batch to take
-		# a "step" in the correct direction
-		W += -args["alpha"] * gradient
- 
-	# update our loss history list by taking the average loss
-	# across all batches
-	lossHistory.append(np.average(epochLoss))
+    # initialize the total loss for the epoch
+    epochLoss = []
 
-    # compute the line of best fit by setting the sigmoid function
+    # loop over our data in batches
+    for (batchX, batchY) in next_batch(X, y, args["batch_size"]):
+        # take the dot product between our current batch of
+        # features and weight matrix `W`, then pass this value
+        # through the sigmoid activation function
+        preds = sigmoid_activation(batchX.dot(W))
+
+        # now that we have our predictions, we need to determine
+        # our `error`, which is the difference between our predictions
+        # and the true values
+        error = preds - batchY
+
+        # given our `error`, we can compute the total loss value on
+        # the batch as the sum of squared loss
+        loss = np.sum(error ** 2)
+        epochLoss.append(loss)
+
+        # the gradient update is therefore the dot product between
+        # the transpose of our current batch and the error on the
+        # # batch
+        gradient = batchX.T.dot(error) / batchX.shape[0]
+
+        # use the gradient computed on the current batch to take
+        # a "step" in the correct direction
+        W += -args["alpha"] * gradient
+
+    # update our loss history list by taking the average loss
+    # across all batches
+    lossHistory.append(np.average(epochLoss))
+
+# compute the line of best fit by setting the sigmoid function
 # to 0 and solving for X2 in terms of X1
 Y = (-W[0] - (W[1] * X)) / W[2]
- 
+
 # plot the original data along with our line of best fit
 plt.figure()
 plt.scatter(X[:, 1], X[:, 2], marker="o", c=y)
 plt.plot(X, Y, "r-")
- 
+
 # construct a figure that plots the loss over time
 fig = plt.figure()
 plt.plot(np.arange(0, args["epochs"]), lossHistory)
